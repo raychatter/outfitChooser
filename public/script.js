3aaf57a483cd6def64e5076a4c0943a6bf2df11f
raychatter <rachel.e.walker@gmail.com>
2015-09-23 18:34:57 -0700

    Now there are shoes

--- public/script.js
+++ public/script.js
@@ -1,22 +1,19 @@
 // TODO: Remove jQuery since you can't access SVG elements from it
 $(document).ready(function(){
-	
-	if($('#top-main-color').text()) {
-		var topImageSVG = document.getElementById("top-image");
-		topImageSVG.addEventListener("load",function() {
-			var topImageSVGDoc = topImageSVG.contentDocument;
-			var insideTopSVGItem = topImageSVGDoc.getElementById("insideTop");
-			insideTopSVGItem.setAttribute("fill", $('#top-main-color').text());
-		});
-	}
-
-	if($('#bottom-main-color').text()) {
-		var bottomImageSVG = document.getElementById("bottom-image");
-		bottomImageSVG.addEventListener("load",function() {
-			var bottomImageSVGDoc = bottomImageSVG.contentDocument;
-			var insidePantsSVGItem = bottomImageSVGDoc.getElementById("InsidePants");
-			insidePantsSVGItem.setAttribute("fill", $('#bottom-main-color').text());
-		});
-	}
+	fillSVG('#top-main-color', "top-image", "insideTop");
+	fillSVG('#bottom-main-color', 'bottom-image', 'InsidePants');
+	fillSVG('#shoe-main-color', 'shoe-image-left', 'ShoeOutline');
+	fillSVG('#shoe-main-color', 'shoe-image-right', 'ShoeOutline');
 });
+
+function fillSVG(colorId, objectId, selectorToFill) {
+	if($(colorId).text()) {
+		var SVGImage = document.getElementById(objectId);
+		SVGImage.addEventListener("load",function() {
+			var SVGDocument = SVGImage.contentDocument;
+			var SVGItem = SVGDocument.getElementById(selectorToFill);
+			SVGItem.setAttribute("fill", $(colorId).text());
+		});
+	}
+}
  
