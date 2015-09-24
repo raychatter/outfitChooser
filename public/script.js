cac2c2c621d20f618fa24284385e57426694014d
raychatter <rachel.e.walker@gmail.com>
2015-09-24 09:06:14 -0700

    Removed jQuery and my page got hella slow

--- public/script.js
+++ public/script.js
@@ -1,19 +1,21 @@
-// TODO: Remove jQuery since you can't access SVG elements from it
-$(document).ready(function(){
-	fillSVG('#top-main-color', "top-image", "insideTop");
-	fillSVG('#bottom-main-color', 'bottom-image', 'InsidePants');
-	fillSVG('#shoe-main-color', 'shoe-image-left', 'ShoeOutline');
-	fillSVG('#shoe-main-color', 'shoe-image-right', 'ShoeOutline');
-});
+window.addEventListener ? 
+window.addEventListener("load",colorSVGs,false) : 
+window.attachEvent && window.attachEvent("onload",colorSVGs);
+
+function colorSVGs(){
+	fillSVG('top-main-color', "top-image", "insideTop");
+	fillSVG('bottom-main-color', 'bottom-image', 'InsidePants');
+	fillSVG('shoe-main-color', 'shoe-image-left', 'ShoeOutline');
+	fillSVG('shoe-main-color', 'shoe-image-right', 'ShoeOutline');
+};
 
 function fillSVG(colorId, objectId, selectorToFill) {
-	if($(colorId).text()) {
-		var SVGImage = document.getElementById(objectId);
-		SVGImage.addEventListener("load",function() {
-			var SVGDocument = SVGImage.contentDocument;
-			var SVGItem = SVGDocument.getElementById(selectorToFill);
-			SVGItem.setAttribute("fill", $(colorId).text());
-		});
+	var $colorId = document.getElementById(colorId);
+	if($colorId) {
+		var $SVGImage = document.getElementById(objectId);
+		var SVGDocument = $SVGImage.contentDocument;
+		var $SVGItem = SVGDocument.getElementById(selectorToFill);
+		$SVGItem.setAttribute("fill", $colorId.textContent);
 	}
 }
  
